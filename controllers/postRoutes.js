dd2c7798523f0779239b5c468f0967146d241f0d
Charles Chavis <chavisca@gmail.com>
2023-09-12 21:18:37 -0400

    changed postRoutes to axios friendly codes

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', async event => {
 
     const vibetype = document.querySelector('#vibetype').value; //Needs passed through tag to equal HTML tag
     const contentbody = document.querySelector('#contentbody').value; // Needs passed through tag to equal HTML tag
-    const userID = req.session.user_id;
+    const userID = req.session.user_id; //Check to make sure this is being captured
 
     const postData = {
         user_id: userID, //Needs to pull in the User ID from the session/cookie
@@ -17,23 +17,22 @@ form.addEventListener('submit', async event => {
         contentbody: contentbody,
         };
         
-        res.render('login');
-        });
-
-    await axios.post(postData),
-    {
+    try {
+    const response = await axios.post('/api/posts', postData, {
         headers: {
         'Content-Type': 'application/json'
         }
+    });
+
+    if (response.status === 200) {
+        console.log('Post successful');
+    } else {
+        console.error('Post failed');
     }
-    .then(function (response) {
-        response.status(200).json(response);
-        console.log(response);
-    })
-    .catch(function (error) {
-        response.status(500).json(error);
-        console.log(error);
-    })
+    } catch (error) {
+        console.error(error);
+    }
+});
 
     Post.create(req.body)
     .then((postData) => {
@@ -44,41 +43,54 @@ form.addEventListener('submit', async event => {
     response.status(400).json(err);
     });
 
-    router.put('/:post_id', (req, res) => {
-        Post.update(
-          {
+    router.put('/:post_id', async (req, res) => {
+        try {
+          const postId = req.params.post_id;
+          const postData = {
             vibetype: req.body.vibetype,
             contentbody: req.body.contentbody,
-          },
-          {
-            where: {
-              post_id: req.params.id,
-            },
-          }
-        )
-          .then((updatedPost) => {
-            res.json(updatedPost);
-          })
-          .catch((err) => {
-            console.log(err);
-            res.json(err);
+          };
+      
+          // Make the axios PUT request
+          const response = await axios.put(`/api/posts/${postId}`, postData, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
           });
+      
+          // Handle the response or send a JSON response here
+          if (response.status === 200) {
+            // Success, handle accordingly
+            // You can render a template or send a JSON response here
+            res.json(response.data); // Send back the updated data if needed
+          } else {
+            // Handle other response status codes
+            res.status(response.status).json(response.data);
+          }
+        } catch (error) {
+          // Handle errors
+          console.error(error);
+          res.status(500).json(error);
+        }
       });
 
-    router.delete('/:post_id', (req, res) => {
-        // Looks for the books based book_id given in the request parameters
-        Post.destroy({
+    router.delete('/:post_id', async (req, res) => {
+        try {
+            const deletedPost = await Post.destroy({
           where: {
             post_id: req.params.id,
           },
         })
-          .then((deletedPost) => {
-            res.json(deletedPost);
-          })
-          .catch((err) => res.json(err));
-      });
-
-
+          if (deletedPost) {
+            res.json({ message: 'Post deleted successfully' });
+          } else {
+            res.status(404).json({ message: 'Post not found' });
+          }
+          } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Internal server error' });
+          }  
+        });
 
 // const options = {
 //     url: 'http://localhost:3000/api/home',
