b37406d7232bc7b67abe3445e927e7a44ff624a1
RVanVlietII <rcvanvlietii@gmail.com>
2023-09-13 14:36:48 -0400

    changes to server.js for running server online, added getRoutes to homeRoutes.js, added brackets to sequelize in post.js

--- models/Post.js
+++ models/Post.js
@@ -25,13 +25,15 @@ Post.init({
       len: [0, 1000], // Adjust the minimum and maximum length as needed
     },
   },
-
+},
+  {
   sequelize,
   timestamps: true,
   freezeTableName: true,
   underscored: true,
   modelName: "post",
-});
+  },
+);
 
 Post.belongsTo(User, {
   foreignKey: "user_id", // The field in the Post model that references User
