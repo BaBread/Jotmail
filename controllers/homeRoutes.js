d7e60075a41edebc99a50bf0169a45d19aea311f
Charles Chavis <chavisca@gmail.com>
2023-09-14 21:34:44 -0400

    updated homeroutes JS and homepage handlebar for login testing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,13 +16,15 @@ router.get('/', async (req, res) => {
 }
 });
 
-router.get('/blogPost', async (req, res) => {
-  try {
-    res.render('blogPost', { user: { user }});
-  } catch (err) {
-    res.status(500).json(err);
+router.get('/login', (req, res) => {
+  // If a session exists, redirect the request to the homepage
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
   }
-})
+
+  res.render('login');
+});
 
 module.exports = router;
 
